1919f02f95024c9609ff3b17de0ec8fb1ab12f14
Turner Hayes <dev@turnerhayes.com>
2018-12-03 18:00:35 -0500

    Add ESLint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "extends": "standard",
+    "rules": {
+        "indent": ["error", "tab"],
+        "no-tabs": "off",
+        "semi": ["error", "always"],
+        "one-var": "off",
+    },
+};
